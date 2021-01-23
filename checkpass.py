f1deee4d3d18b569bf7d7d4937433f6e0e7c75ca
Nikola Jevtovic <nikolejevtovica@gmail.com>
2021-01-23 12:01:01 +0100

    implement API request

--- checkpass.py
+++ checkpass.py
@@ -1,4 +1,17 @@
 # Start that project
 
+import requests
 
- 
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching: {res.status_code}, check the API and try again")
+    return res
+def pwned_api_check(password):
+    # Check password if it exist in API response
+    pass
+
+request_api_data('123')
